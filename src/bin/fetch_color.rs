5fee26db5dc482013d705d97353039af47afcafa
toyama <toyama@toyama1710.net>
2025-12-21 03:53:43 +0900

    extract pixels as CIE Lab (each element treat as u16)

--- src/bin/fetch_color.rs
+++ src/bin/fetch_color.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use image::{ColorType, ImageReader};
+use image::{ColorType, ImageBuffer, ImageDecoder, ImageReader, Rgba};
+use moxcms::{ColorProfile, Layout, TransformOptions};
+use std::fs::File;
+use std::io::{BufReader, Seek};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -43,16 +46,49 @@ impl Config {
     }
 }
 
+fn extract_icc_chunk(file: &File) -> Result<Option<Vec<u8>>, anyhow::Error> {
+    let reader = ImageReader::new(BufReader::new(file)).with_guessed_format()?;
+    let mut decoder = reader.into_decoder()?;
+    let icc_chunk = decoder.icc_profile()?;
+    Ok(icc_chunk)
+}
+
+// always return rgba16 even if the source image has 8-bit color
+fn extract_pixels(file: &File) -> Result<ImageBuffer<Rgba<u16>, Vec<u16>>, anyhow::Error> {
+    let reader = ImageReader::new(BufReader::new(file)).with_guessed_format()?;
+    let img = reader.decode()?;
+    let pixels = img.to_rgba16();
+    Ok(pixels)
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
     let config = Config::new(args)?;
-    let img = ImageReader::open(config.path)?.decode()?;
 
-    // TODO: use config.color_type in future
-    let pixels = img.to_rgba8();
+    let mut file = File::open(config.path)?;
+    let icc_chunk = extract_icc_chunk(&file)?;
+    file.rewind()?; // fail extract_pixels if not rewind
+    let pixels = extract_pixels(&file)?.into_raw();
 
-    for pixel in pixels.iter() {
-        println!("{:?}", pixel);
+    let src_profile = if let Some(icc_chunk) = icc_chunk {
+        ColorProfile::new_from_slice(&icc_chunk)?
+    } else {
+        ColorProfile::new_srgb()
+    };
+
+    let transform = src_profile.create_transform_16bit(
+        Layout::Rgba,
+        &ColorProfile::new_lab(),
+        Layout::Rgb,
+        TransformOptions::default(),
+    )?;
+
+    println!("test1");
+    let mut lab_pixels = vec![0; 3 * pixels.len() / 4];
+    transform.transform(&pixels, &mut lab_pixels)?;
+    println!("test2");
+    for lab in lab_pixels.chunks(3) {
+        println!("L: {}, a: {}, b: {}", lab[0], lab[1], lab[2]);
     }
 
     Ok(())
